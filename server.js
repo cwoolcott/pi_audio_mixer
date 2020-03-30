847c620f0f60634b55cd853f595199d141729283
Chris_remote <chris.woolcott@quotacy.com>
2020-03-30 13:44:33 -0500

    audio_init

--- server.js
+++ server.js
@@ -91,6 +91,41 @@ function audioTest(option) {
   else if (option === 'Get Host') {
     console.log(portAudio.getHostAPIs());
   }
+  else if (option === 'Audio Mixer') {
+    var AudioMixer = require('audio-mixer');
+
+    // Creates a new audio mixer with the specified options
+    let mixer = new AudioMixer.Mixer({
+      channels: 2,
+      bitDepth: 16,
+      sampleRate: 44100,
+      clearInterval: 250
+    });
+
+    // Creates an input that is attached to the mixer
+    let input = mixer.input({
+      channels: 1,
+      volume: 75
+    });
+
+    // Creates a standalone input
+    let standaloneInput = new AudioMixer.Input({
+      channels: 1,
+      bitDepth: 16,
+      sampleRate: 48000,
+      volume: 75
+    });
+
+    // Adds the standalone input to the mixer
+    mixer.addInput(standaloneInput);
+
+    // Pipes a readable stream into an input
+    deviceInputStream.pipe(input);
+    deviceInputStream2.pipe(standaloneInput);
+
+    // Pipes the mixer output to an writable stream
+    mixer.pipe(deviceOutputStream);
+  }
 }
 
 function menu() {
@@ -101,7 +136,7 @@ function menu() {
         type: 'list',
         name: 'option',
         message: 'TESTING:',
-        choices: ['Get Audio Device', 'Get Host', 'Load Sound Card', 'Sound Player', 'Play Audio', 'Audio IO', 'Exit'],
+        choices: ['Audio Mixer', 'Get Audio Device', 'Get Host', 'Load Sound Card', 'Sound Player', 'Play Audio', 'Audio IO', 'Exit'],
       },
     ])
     .then(answers => {
